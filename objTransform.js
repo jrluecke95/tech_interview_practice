29fa237db85720d6b9712852b68a0f139f468d4f
Jake Luecke <jrluecke95@gmail.com>
2021-03-19 14:30:15 -0400

    first problem done

---
+++ objTransform.js
@@ -0,0 +1,27 @@
+const object = {
+  person: { name: 'Max', age: 28 },
+  hobbies: { sport: 'Football', game: 'Call of Duty' },
+  food: { asian: 'Pad Thai', mexican: 'Birria Taco', american: 'Hamburgers' }
+  }
+// without internet help
+function objTransform(object) {
+  let values = Object.values(object);
+  let newArray = []
+  for (let i = 0; i < values.length; i++) {
+    newArray.push(values[i])
+  }
+  return newArray
+}
+
+console.log(objTransform(object))
+
+// second solution using internet help
+function objTransform2(object) {
+  let newArray = [];
+  for (value in object) {
+    newArray.push(object[value])
+  }
+  return newArray;
+}
+
+console.log(objTransform2(object))
